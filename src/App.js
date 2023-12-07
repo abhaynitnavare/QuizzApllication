8f652274e37a472c8e80766ce19d92c617fd0676
abhaynitnavare <132355288+abhaynitnavare@users.noreply.github.com>
2023-12-07 18:40:54 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+
+import {BrowserRouter as Router,Route,Routes } from "react-router-dom"
+import './App.css';
+import Profile from "./Components/Profile";
+import NavlinkPage from './Components/Navbar';
+import FirstSlide from './Components/FirstSlide';
+import LoginComponent from "./Components/Login"
+import SignupComponent from "./Components/Signup"
+import LeadBordComponent from "./Components/Leadbord"
+import HomeComponent from './Components/Home';
+import QuizApp from'./Components/Quizquestion';
+function App() {
+  return (
+    <div className="App">
+      <Router>
+          <Routes>
+            <Route path='/' element={<FirstSlide/>}/>
+            <Route path='Navlink' element={<NavlinkPage />}/>
+            <Route path='Login' element={<LoginComponent/>}/>
+            <Route path='Signup' element={<SignupComponent/>}/>
+            <Route path='Leadbord' element={<LeadBordComponent/>}/>
+            <Route path='Home' element={<HomeComponent />}/>
+            <Route path='Profile' element={<Profile />}/>
+            <Route path="/quizQuestion" element={<QuizApp />}/>
+          </Routes>
+      </Router>
+
+    </div>
+  );
+}
+
+
+export default App;
